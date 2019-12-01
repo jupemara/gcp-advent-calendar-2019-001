1b6d48fdc929a26bbc081f905d920b38463c8d11
ARASHI, Jumpei <jumpei.arashi@arashike.com>
2019-12-02 02:44:37 +0900

    npm install -S firebase-admin

--- test/domain/model/user/user-id.ts
+++ test/domain/model/user/user-id.ts
@@ -1,5 +1,5 @@
 import test from 'ava';
-import { UserId } from '../../../../src/domain/model/user/UserId';
+import { UserId } from '../../../../src/domain/model/user/user-id';
 
 test('UserId.constractor with valid user id, it succeeds to create new UserId instance', t => {
   const value = 'user-id_001',
