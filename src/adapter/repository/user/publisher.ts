affb90ae471a6a00631bd1d2880dee3bbc339fe6
ARASHI, Jumpei <jumpei.arashi@arashike.com>
2019-12-02 16:50:22 +0900

    implement Pub/Sub publisher as messaging publisher

---
+++ src/adapter/repository/user/publisher.ts
@@ -0,0 +1,10 @@
+import { IPublisher } from '../../../domain/model/user/publisher';
+import { RegisterUserRequest } from '../../../domain/model/user/register-user-request';
+import { Topic } from '@google-cloud/pubsub';
+
+export class RegisterUserPublisher implements IPublisher {
+  constructor(private readonly pubsubTopic: Topic) {}
+  public async publish(request: RegisterUserRequest): Promise<void> {
+    await this.pubsubTopic.publish(Buffer.from(JSON.stringify(request)));
+  }
+}
